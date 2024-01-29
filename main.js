754afc4e3cbc2d7fef70e2004ea33d6ac64a9e00
Adriatik Berdufi <adratik.berdufi@gmail.com>
2024-01-29 15:44:04 +0100

    add img

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ createApp ({
     data() {
         return {
             text : `Hello Vue`,
+            img: `https://img.freepik.com/free-photo/concept-nebula-with-galaxies-deep-space-cosmos-discovery-outer-space-stars-universe_90220-1141.jpg`,
         }
     }
 }).mount('#app')
